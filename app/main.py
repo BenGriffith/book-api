c0ff44d51c9dc7781c477031ed189dad36b53329
Ben Griffith <bengriffith@outlook.com>
2022-05-12 22:10:54 -0500

    starting to update code for db component

--- app/main.py
+++ app/main.py
@@ -1,41 +1,89 @@
-from http.client import HTTPException
+from sqlmodel import SQLModel, Session, select
 from fastapi import FastAPI, HTTPException
 
-from app.models import Book
+from db import create_db_and_tables, engine
+from models import Books, BooksRead, BooksUpdate
+
 
 app = FastAPI()
 
-fake_db = {}
-
-@app.get("/")
-def root():
-    return dict(message = "Welcome to the Book API!")
+# fake_db = {}
 
 
-@app.get("/books/{book_id}", response_model=Book, status_code=200)
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
+
+
+# @app.get("/")
+# def root():
+#     return dict(message = "Welcome to the Book API!")
+
+
+@app.get("/books/", response_model=list[Books], status_code=200)
+def get_books():
+    with Session(engine) as session:
+        books = session.exec(select(Books)).all()
+        return books
+
+
+# @app.get("/books/{book_id}", response_model=Book, status_code=200)
+# def get_book(book_id: int):
+#     return fake_db[book_id]
+
+
+@app.get("/books/{book_id}", response_model=BooksRead, status_code=200)
 def get_book(book_id: int):
-    return fake_db[book_id]
+    with Session(engine) as session:
+        book = session.get(Books, book_id)
+        if not book:
+            raise HTTPException(status_code=404, detail="Book not found")
+        return book
 
 
-@app.post("/books/", response_model=Book, status_code=201)
-def create_book(book: Book):
-    fake_db[book.id] = book
-    return book
+# @app.post("/books/", response_model=Book, status_code=201)
+# def create_book(book: Book):
+#     fake_db[book.id] = book
+#     return book
 
 
-@app.put("/books/{book_id}", response_model=Book)
-def update_book(book_id: int, book: Book):
-    if book_id not in fake_db:
-        raise HTTPException(status_code=404, detail="Book not found")
+@app.post("/books/", response_model=Books, status_code=201)
+def create_book(book: Books):
+    with Session(engine) as session:
+        session.add(book)
+        session.commit()
+        session.refresh(book)
+        return book
+
+
+# @app.put("/books/{book_id}", response_model=Book)
+# def update_book(book_id: int, book: Book):
+#     if book_id not in fake_db:
+#         raise HTTPException(status_code=404, detail="Book not found")
     
-    fake_db[book.id] = book
-    return fake_db[book.id]        
+#     fake_db[book.id] = book
+#     return fake_db[book.id]
 
 
-@app.delete("/books/{book_id}")
-def delete_book(book_id: int):
-    if not book_id in fake_db:
-        raise HTTPException(status_code=404, detail="Book not found")
+@app.patch("/books/{book_id}", response_model=BooksRead)
+def update_book(book_id: int, book: BooksUpdate):
+    with Session(engine) as session:
+        book_record = session.get(Books, book_id)
+        if not book_record:
+            raise HTTPException(status_code=404, detail="Book not found")
+        book_data = book.dict(exclude_unset=True)
+        for key, value in book_data.items():
+            setattr(book_record, key, value)
+        session.add(book_record)
+        session.commit()
+        session.refresh(book_record)
+        return book_record
 
-    del fake_db[book_id]
-    return {"ok": True}
+
+# @app.delete("/books/{book_id}")
+# def delete_book(book_id: int):
+#     if not book_id in fake_db:
+#         raise HTTPException(status_code=404, detail="Book not found")
+
+#     del fake_db[book_id]
+#     return {"ok": True}
--- app/models.py
+++ app/models.py
@@ -2,18 +2,37 @@ from datetime import date, datetime
 from typing import Optional
 
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field
 
 
-class Book(BaseModel):
-
-    id: int
+class BooksBase(SQLModel):
     title: str
-    authors: list[str]
-    # publisher: str
-    # published_date: date
-    # description: str
-    # page_count: int
-    # categories: list[str]
-    # average_rating: float
-    # ratings_count: int
-    # url: str
+    authors: str
+
+
+class Books(BooksBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class BooksRead(BooksBase):
+    id: int
+
+
+class BooksUpdate(BooksBase):
+    title: str
+    authors: str
+
+
+# class Book(BaseModel):
+
+#     id: int
+#     title: str
+#     authors: list[str]
+#     publisher: str
+#     published_date: date
+#     description: str
+#     page_count: int
+#     categories: list[str]
+#     average_rating: float
+#     ratings_count: int
+#     url: str
