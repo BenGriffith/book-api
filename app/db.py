84b5d3255bc35e3d91485d41fbaaa284c52a844c
Ben Griffith <bengriffith@outlook.com>
2022-05-14 22:39:08 -0500

    refactored use of session and updated test cases for books model

--- app/db.py
+++ app/db.py
@@ -1,10 +1,16 @@
-from sqlmodel import SQLModel, create_engine
+from sqlmodel import Session, SQLModel, create_engine
 
 
 sqlite_file_name = "database.db"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 
 engine = create_engine(sqlite_url, echo=False)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from sqlmodel import Session, select
-from fastapi import FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException
 
-from db import create_db_and_tables, engine
-from models import Books, BooksRead, BooksUpdate
+from app.db import get_session, create_db_and_tables
+from app.models import Books, BooksRead, BooksUpdate
 
 
 app = FastAPI()
@@ -10,56 +10,67 @@ app = FastAPI()
 
 @app.on_event("startup")
 def on_startup():
+
     create_db_and_tables()
 
 
 @app.get("/")
 def root():
+
     return dict(message = "Welcome to the Book API!")
 
 
 @app.get("/books/", response_model=list[Books], status_code=200)
-def get_books():
+def get_books(*, session: Session = Depends(get_session)):
 
-    with Session(engine) as session:
         books = session.exec(select(Books)).all()
         return books
 
 
 @app.get("/books/{book_id}", response_model=BooksRead, status_code=200)
-def get_book(book_id: int):
+def get_book(*, session: Session = Depends(get_session), book_id: int):
 
-    with Session(engine) as session:
-        book = session.get(Books, book_id)
-        if book is None:
-            raise HTTPException(status_code=404, detail="Book not found")
-        return book
+    book = session.get(Books, book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+    return book
 
 
 @app.post("/books/", response_model=Books, status_code=201)
-def create_book(book: Books):
+def create_book(*, session: Session = Depends(get_session), book: Books):
 
-    with Session(engine) as session:
-        session.add(book)
-        session.commit()
-        session.refresh(book)
+    session.add(book)
+    session.commit()
+    session.refresh(book)
 
-        return book
+    return book
 
 
 @app.patch("/books/{book_id}", response_model=BooksRead)
-def update_book(book_id: int, book: BooksUpdate):
+def update_book(*, session: Session = Depends(get_session), book_id: int, book: BooksUpdate):
 
-    with Session(engine) as session:
-        book_record = session.get(Books, book_id)
-        if book_record is None:
-            raise HTTPException(status_code=404, detail="Book not found")
+    book_record = session.get(Books, book_id)
+    if book_record is None:
+        raise HTTPException(status_code=404, detail="Book not found")
 
-        book_record.title = book.title
-        book_record.authors = book.authors
+    book_record.title = book.title
+    book_record.authors = book.authors
 
-        session.add(book_record)
-        session.commit()
-        session.refresh(book_record)
+    session.add(book_record)
+    session.commit()
+    session.refresh(book_record)
 
-        return book_record
+    return book_record
+
+
+@app.delete("/books/{book_id}")
+def delete_book(*, session: Session = Depends(get_session), book_id: int):
+
+    book = session.get(Books, book_id)
+    if book is None:
+        raise HTTPException(status_code=404, detail="Book not found")
+
+    session.delete(book)
+    session.commit()
+
+    return {"ok": True}
