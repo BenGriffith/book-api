4f39819070aa9c84d445f2f633126c6ac3a9ab81
Bob Belderbos <bobbelderbos@gmail.com>
2022-06-20 16:59:32 +0200

    test pr

--- app/db.py
+++ app/db.py
@@ -8,8 +8,14 @@ DATABASE_URL = config("DATABASE_URL", cast=str)
 SECRET_KEY = config("SECRET_KEY", cast=str)
 ALGORITHM = config("ALGORITHM", cast=str)
 ACCESS_TOKEN_EXPIRE_MINUTES = config("ACCESS_TOKEN_EXPIRE_MINUTES", cast=int)
+CHECK_SAME_THREAD = config("CHECK_SAME_THREAD", default=False, cast=bool)
 
-engine = create_engine(DATABASE_URL, echo=DEBUG)
+if CHECK_SAME_THREAD:
+    kwargs = {"check_same_thread": False}
+else:
+    kwargs = {}
+engine = create_engine(DATABASE_URL, echo=DEBUG, **kwargs)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
